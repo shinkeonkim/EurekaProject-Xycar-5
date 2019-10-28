b57a0df66cfc9cce25de3415d1d741e1e250d64d
shinkeonkim <singun11@naver.com>
2019-10-28 17:48:08 +0900

    add ultrasonic filter

--- src/obstacleDetection/obstacleDetection.py
+++ src/obstacleDetection/obstacleDetection.py
@@ -37,29 +37,45 @@ if __name__ == '__main__':
 
     rate = rospy.Rate(10)
     forward = True
+    
+    front = []
+    back = []
 
     while not rospy.is_shutdown():
+        
+        if len(front) > 4:
+            front.pop(0)
+        if len(back) > 4:
+            back.pop(0)
+        front.append(usonic_data[1])
+        back.append(usonic_data[4])
+        
+        front_value = sum(front) / len(front)
+        back_value = sum(back) / len(back)
+        
         if forward:
-	    if usonic_data[1] <= 60:
-	        for stop_cnt in range(2):
+            if front_value <= 40:
+                drive(90,90)
+                time.sleep(5)
+                for stop_cnt in range(2):
                     drive(90, 90)
                     time.sleep(0.1)
-                    drive(90, 60)
+                    drive(90, 70)
                     time.sleep(0.1)
-                time.sleep(0.5)
                 forward = False
-	    else:
-	        drive(90, 120)
+            else:
+                drive(90, 110)
         else:
-            if usonic_data[4] <= 60:
-	        for stop_cnt in range(2):
+            if back_value <= 40:
+                drive(90,90)
+                time.sleep(5)
+                for stop_cnt in range(2):
                     drive(90, 90)
                     time.sleep(0.1)
-                    drive(90, 120)
+                    drive(90, 110)
                     time.sleep(0.1)
-                time.sleep(0.5)
                 forward = True
-	    else:
-	        drive(90, 60)
+            else:
+	            drive(90, 70)
         rate.sleep()
     rospy.on_shutdown(exit_node)
