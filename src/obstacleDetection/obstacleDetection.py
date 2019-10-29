b6c525cf1283a144d13b13ef341707a81e50760a
shinkeonkim <singun11@naver.com>
2019-10-29 17:55:01 +0900

    middle filter

--- src/obstacleDetection/obstacleDetection.py
+++ src/obstacleDetection/obstacleDetection.py
@@ -7,6 +7,12 @@ from std_msgs.msg import Int32MultiArray
 motor_pub = None
 usonic_data = None
 
+front = []
+back = []
+
+front_ema = []
+back_ema = []
+
 # init_node
 def init_node():
     global motor_pub
@@ -28,8 +34,9 @@ def drive(angle, speed):
 # callback
 def callback(data):
     global usonic_data
-    usonic_data = data.data
 
+    usonic_data = data.data
+    
 # sliding window moving average
 def swma(vals):
     return sum(vals) / len(vals)
@@ -51,24 +58,18 @@ if __name__ == '__main__':
     init_node()
     time.sleep(3)
 
+    
     rate = rospy.Rate(10)
     forward = True
     
     forward_cnt = 0
-    forward_speed = [110,120,130]
-    forward_std_value = [40,50,60]
+    forward_speed = [110,117,123]
+    forward_std_value = [40,50,64]
 
     backward_speed = 70
-    back_std_value = 40
-
-    front = []
-    back = []
-
-    front_ema = []
-    back_ema = []
+    back_std_value = 43
 
     while not rospy.is_shutdown():
-        
         if len(front) > 4:
             front.pop(0)
         if len(back) > 4:
@@ -76,13 +77,10 @@ if __name__ == '__main__':
         front.append(usonic_data[1])
         back.append(usonic_data[4])
         
-        front_value = sum(front) / len(front)
-        back_value = sum(back) / len(back)
-        
         if forward:
             #if ema(front_ema, front) <= forward_std_value[forward_cnt]:
-            #if med(front) <= forward_std_value[forward_cnt]:
-            if swma(front) <= forward_std_value[forward_cnt]:
+            if med(front) <= forward_std_value[forward_cnt]:
+            #if swma(front) <= forward_std_value[forward_cnt]:
                 drive(90,90)
                 time.sleep(5)
                 for stop_cnt in range(2):
