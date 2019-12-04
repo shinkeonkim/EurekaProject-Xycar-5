4e5c92cc443ffc313537e5fdf16215104f89ba21
dawit <dawit@kookmin.ac.kr>
2019-12-04 15:28:51 +0900

    ad_project commit

--- src/ad_project/src/autodrive.py
+++ src/ad_project/src/autodrive.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import rospy, time
+import math
+import time
 
 from linedetector import LineDetector
 from obstacledetector import ObstacleDetector
@@ -13,32 +15,150 @@ class AutoDrive:
         self.line_detector = LineDetector('/usb_cam/image_raw')
         self.obstacle_detector = ObstacleDetector('/ultrasonic')
         self.driver = MotorDriver('/xycar_motor_msg')
+        self.slow_time = time.time()
+        self.prev_dis = 100
+        self.prev_l = []
+        self.prev_m = []
+        self.prev_r = []
+        self.MAX_SIZE = 3
+
+    def average(self, L):
+        if len(L) == 0:
+            return 100
+        return sum(L) / len(L)
 
     def trace(self):
         obs_l, obs_m, obs_r = self.obstacle_detector.get_distance()
-        line_l, line_r = self.line_detector.detect_lines()
-        self.line_detector.show_images(line_l, line_r)
-        angle = self.steer(line_l, line_r)
-        speed = self.accelerate(angle, obs_l, obs_m, obs_r)
-        self.driver.drive(angle + 90, speed + 90)
+        line_theta,left,right = self.line_detector.detect_lines()
+        line_theta += 0.4
+        angle = self.steer(line_theta,left,right)
+        speed = self.accelerate(angle,line_theta,left,right)
+        print(line_theta)
+        #print(line_theta, left, right)
+        print("left: {} mid: {} right: {}".format(obs_l,obs_m,obs_r))
+        cnt = 0
+        s = 0
+        cos_theta = 0.9
+        dis = self.prev_dis
+        obs_l *= cos_theta
+        obs_r *= cos_theta
+        if obs_l != 0:
+            if len(self.prev_l) + 1 >= self.MAX_SIZE:
+                self.prev_l.pop(0)
+            self.prev_l.append(obs_l)
+            if obs_l <= 70:
+                cnt +=1
+            s += obs_l
+        if obs_m != 0:
+            if len(self.prev_m) + 1 >= self.MAX_SIZE:
+                self.prev_m.pop(0)
+            self.prev_m.append(obs_m)
+            if obs_m <= 70:
+                cnt +=2
+            s += obs_m
+        if obs_r != 0:
+            if len(self.prev_r) + 1 >= self.MAX_SIZE:
+                self.prev_r.pop(0)
+            self.prev_r.append(obs_r)
+            if obs_r <=70:
+                cnt +=1
+            s += obs_r
+        if cnt >=2:
+            dis = s/cnt
 
-    def steer(self, left, right):
-        mid = (left + right) // 2
-        if mid < 280:
-            angle = -30
-        elif mid > 360:
-            angle = 30
+        if ((cnt >=3) or (obs_m != 0 and obs_m <=70 and self.average(self.prev_m) <= 75)) or (self.average(self.prev_l) <= 75 and self.average(self.prev_m) <= 75 and self.average(self.prev_r) <=75):
+            for i in range(2):
+                self.driver.drive(90,90)
+                time.sleep(0.1)
+                self.driver.drive(90,60)
+                time.sleep(0.1)
+            self.driver.drive(90,90)
+            time.sleep(5)
         else:
-            angle = 0
-        return angle
+            self.driver.drive(angle + 90+ 2.77, speed)
 
-    def accelerate(self, angle, left, mid, right):
-        if min(left, mid, right) < 50:
-            speed = 0
-        elif angle < -20 or angle > 20:
-            speed = 20
+        if cnt >=2:
+            self.prev_dis = dis
+
+    def steer(self, theta, left, right):
+        """
+        weight = 0.0
+
+        if left == -1:
+            weight = 0.0
+
+        elif right == -1:
+            weight = 0.0
         else:
-            speed = 30
+            mid = (left + right) / 2
+            diff = 55-mid
+            
+            if abs(diff) < 3:
+                weight = 0.0
+            # car is at right
+            elif diff < 0:
+                weight = -1.0
+            elif diff > 0:
+                weight = 1.0
+        """
+        K = 0.0
+        if -0.8 <= theta <= 0.8:
+            K = 8.0
+        elif -5 <= theta <= 5:
+            K = 2.0
+        elif -10 <= theta <= 10:
+            K = 2.3
+        elif -15 <= theta <= 15:
+            '''
+            if theta < 0:
+                if theta > -10:
+                    K = 1.86
+                else:
+                    K = 1.4
+            else:
+                K = 1.75
+            '''
+            if theta < 0:
+                K = 1.5
+            else:
+                K = 2.0
+        else: #elif abs(theta) < 30:
+            if theta < 0:
+                K = 3.0
+            else:
+                K = 3.0
+        #else:
+        #    K = 
+        '''
+        elif theta >= 20:
+            K = 3.0
+        elif theta <= -20:
+            K = 2.1
+        else:
+            K = 1.5
+        '''	
+        """
+        if theta > 0:
+            K = 1.75
+        else:
+            K = 1.6
+        """
+        angle = theta * K
+
+        return angle #+ (weight * 15)
+        #return angle
+
+    def accelerate(self, angle, theta, left, right):
+        K = 135
+        
+        if abs(theta) > 4:
+            self.slow_time = time.time() + 2
+
+        if time.time() < self.slow_time:
+            K = 130
+        
+        speed = K# - min(abs(theta)/2, 10) 
+
         return speed
 
     def exit(self):
@@ -47,7 +167,7 @@ class AutoDrive:
 if __name__ == '__main__':
     car = AutoDrive()
     time.sleep(3)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(100)
     while not rospy.is_shutdown():
         car.trace()
         rate.sleep()
--- src/ad_project/src/linedetector.py
+++ src/ad_project/src/linedetector.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+
 import rospy
 import cv2
 import numpy as np
+import math
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
@@ -9,15 +12,26 @@ class LineDetector:
     def __init__(self, topic):
         # Initialize various class-defined attributes, and then...
         self.cam_img = np.zeros(shape=(480, 640, 3), dtype=np.uint8)
+        self.vid_out = cv2.VideoWriter('/home/nvidia/output.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, (640, 480))
+        '''
         self.mask = np.zeros(shape=(self.scan_height, self.image_width),
                              dtype=np.uint8)
         self.edge = np.zeros(shape=(self.scan_height, self.image_width),
                              dtype=np.uint8)
+        '''
         self.bridge = CvBridge()
         rospy.Subscriber(topic, Image, self.conv_image)
+        self.before = np.array([[0, 315], [639, 305], [170, 260], [460, 250]], dtype='float32')
+        self.after = np.array([[0, 100], [100, 100], [0, -100], [100, -100]], dtype='float32')
+        self.theta = 0.0
+
+    def __del__(self):
+        self.vid_out.release()
 
     def conv_image(self, data):
         self.cam_img = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        self.vid_out.write(self.cam_img)
+        '''
         v = self.roi_vertical_pos
         roi = self.cam_img[v:v + self.scan_height, :]
 
@@ -31,10 +45,133 @@ class LineDetector:
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
         self.edge = cv2.Canny(blur, 60, 70)
+        '''
 
     def detect_lines(self):
-        # Return positions of left and right lines detected.
-        return -1, -1
+        #self.theta = 0.0
+        frame = self.cam_img
+        tdSize = (100, 210)
+        m = cv2.getPerspectiveTransform(self.before, self.after)
+        topdown = cv2.warpPerspective(frame, m, tdSize)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (3, 3), 0)
+        edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+        edges = cv2.warpPerspective(edges, m, tdSize)
+        lines = cv2.HoughLines(edges, 1, np.pi / 180, 80)
+        
+        left, right = -1, -1
+        #lCnt, rCnt = 0, 0
+        
+        if lines is not None:
+            cnts = [0 for _ in range(50)]
+            vals = [0.0 for _ in range(len(cnts))]
+            cnt = 0
+
+            ptCnts = [0 for _ in range(10)]
+            ptVals = [0.0 for _ in range(len(ptCnts))]
+
+            for line in lines:
+                for rho, theta in line:
+                    index = int(theta / np.pi * len(cnts))
+                    cnts[index] += 1
+                    vals[index] += theta
+                    cnt += 1
+
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a * rho
+                    y0 = b * rho
+                    '''
+                    x1 = int(x0 + 1000 * (-b))
+                    y1 = int(y0 + 1000 * (a))
+                    x2 = int(x0 - 1000 * (-b))
+                    y2 = int(y0 - 1000 * (a))
+
+                    cv2.line(topdown, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                    '''
+                    t = (199 - y0) / a
+                    x = x0 - t * b
+
+                    index = min(max(int(x / tdSize[0] * len(ptCnts)), 0), len(ptCnts) - 1)
+                    ptCnts[index] += 1
+                    ptVals[index] += x
+                    '''
+                    if x < 50:
+                        left += x
+                        lCnt += 1
+                    else:
+                        right += x
+                        rCnt += 1
+                    '''
+            if cnt > 0:
+                thetas = []
+
+                for i in range(2):
+                    index = cnts.index(max(cnts))
+                    if cnts[index] == 0:
+                        break
+                    theta = vals[index] / cnts[index]
+                    if theta > math.pi / 2:
+                        theta -= math.pi
+                    thetas.append(theta)
+                    cnts[index] = 0
+                theta = sum(thetas) / len(thetas)
+
+                #weight = 0.2
+                self.theta = theta #self.theta * (1.0 - weight) + theta * weight
+
+                pts = []
+                half = len(ptCnts) // 2
+
+                for cnts, vals in [(ptCnts[:half], ptVals[:half]), (ptCnts[half:], ptVals[half:])]:
+                    index = cnts.index(max(cnts))
+                    pt = vals[index] / cnts[index] if cnts[index] > 0 else -1
+                    pts.append(pt)
+
+                left, right = pts
+                '''
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = 50
+                y0 = 100
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+
+                cv2.line(topdown, (x1, y1), (x2, y2), (0, 255, 0), 5)
+                '''
+        else:
+            self.theta *= 0.915
+        '''
+        cv2.imshow("origin", frame)
+        cv2.imshow('hough', topdown)
+        cv2.waitKey(1)
+        '''
+
+        #left = left / lCnt if lCnt > 0 else -1
+        #right = right / rCnt if rCnt > 0 else -1
+        '''
+	if left >= 0:
+            cv2.circle(topdown, (int(left), 199), 3, (0, 0, 255), 3)
+
+        if right >= 0:
+            cv2.circle(topdown, (int(right), 199), 3, (255, 0, 0), 3)
+
+        cv2.imshow('hough', topdown)
+        #cv2.waitKey(1)
+        
+        frame = cv2.circle(frame, (0, 315), 2, (0, 255, 0), 3)
+        frame = cv2.circle(frame, (639, 320), 2, (0, 255, 0), 3)
+
+        frame = cv2.circle(frame, (170, 260), 2, (0, 255, 0), 3)
+        frame = cv2.circle(frame, (460, 250), 2, (0, 255, 0), 3)
+        cv2.imshow('origin', frame)
+        cv2.imshow('edges', edges)
+        cv2.waitKey(1)
+        '''
+        return self.theta * 180 / math.pi, left, right
 
     def show_images(self, left, right):
         # Display images for debugging purposes;
